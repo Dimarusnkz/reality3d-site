342e99aca855d80da4db7a0a81442b5ffccd6b1c
Dmitry <dmitry@reality3d.ru>
2026-03-17 20:54:00 +0300

    fix(shop): prisma relations + generate during server build

--- build_server.sh
+++ build_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /var/www/reality3d
 rm -rf .next
-npx prisma migrate deploy >> build_log.txt 2>&1
+npx prisma migrate deploy > build_log.txt 2>&1
+npx prisma generate >> build_log.txt 2>&1
 npm run build >> build_log.txt 2>&1
 echo "BUILD_FINISHED" >> build_log.txt
